974d208d84558f8307654379b123ce8e1fb75dc1
MohamedAhmed <mohamed.ahmed799999@gmail.com>
2022-01-20 21:43:58 +0200

    end of Pagination Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    ShopModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
